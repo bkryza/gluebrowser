bc6d2a9b100c7ac9a662bebba7d105ddd8f45796
szimin18 <szimin18@gmail.com>
2014-02-02 19:23:58 +0100

    Added final functionality

--- src/gluebrowser/core.clj
+++ src/gluebrowser/core.clj
@@ -18,15 +18,6 @@
 
   ")
 
-(def TODO nil)
-
-(def TOP 20)
-
-(def TAKETOP true)
-
-;(def default-query-dn "GlueSiteUniqueID=CYFRONET-LCG2,Mds-Vo-name=CYFRONET-LCG2,Mds-Vo-name=local,o=grid") ; line 13211
-
-
 (defn query-server
   "
   Connects to the ldap server and executes the query on it passing it any argument list
@@ -60,30 +51,50 @@
                          :list-vo            "List of Virtual Organizations:"})
 
 
-(defn print-list
+(defn print-single-column-list
   "
   Prints a list of results. Prints one result item in a line
   "
-  [items-list]
-  (loop [unprocessed-items-list items-list number 1]
-    (if (not= (count unprocessed-items-list) 0)
-      (do
-        (printf "%5d. %s%n" number (first unprocessed-items-list))
-        (recur (rest unprocessed-items-list) (+ number 1))))))
+  [items]
+  (loop [unprocessed-items-list items number 1]
+    (when (not-empty unprocessed-items-list)
+      (printf (str " %" (inc (Math/log10 (count items))) "d. %s%n") number (first unprocessed-items-list))
+      (recur
+        (rest unprocessed-items-list)
+        (inc number)))))
+
+
+(defn print-double-column-list
+  "
+  Prints a list of results. Prints one result item in a line spreading results into columns
+  "
+  [items]
+  (let [first-column-width (inc (Math/log10 (count items)))
+        second-column-width (reduce max 0 (map #(count (first %)) items))]
+    (loop [unprocessed-items-list items number 1]
+      (when (not-empty unprocessed-items-list)
+        (printf
+          (str " %" first-column-width "d. %-" second-column-width "s %s%n")
+          number
+          (first (first unprocessed-items-list))
+          (second (first unprocessed-items-list)))
+        (recur
+          (rest unprocessed-items-list)
+          (inc number))))))
 
 
 ;
-; Set of all command line attributes which handle queries
+; Set of all command line attributes which handle list queries
 ;
-(def list-query-cli-args #{ :list-sites
-                            :list-services
-                            :list-clusters
-                            :list-subclusters
-                            :list-ce
-                            :list-software
-                            :list-se
-                            :list-sa
-                            :list-vo})
+(def list-query-cli-args #{  :list-sites
+                             :list-services
+                             :list-clusters
+                             :list-subclusters
+                             :list-ce
+                             :list-software
+                             :list-se
+                             :list-sa
+                             :list-vo})
 
 
 (defn split-attributes
@@ -91,18 +102,7 @@
   Splits human-defind attributes into query-readable form
   "
   [attributes-string]
-  (filter #(not= % (keyword "")) (map #(keyword %) (split attributes-string #",|;"))))
-
-;(defn split-attributes
-;  "
-;  Splits human-defind attributes into query-readable form
-;  "
-;  [attributes-string]
-;  (map
-;    #(keyword %)
-;    (if-let [splitted-attributes (split attributes-string #",;")]
-;      splitted-attributes
-;      [])))
+  (filter #(not= % (keyword "")) (split attributes-string #",|;")))
 
 
 (defn dispatch-cli-queries
@@ -111,25 +111,38 @@
   "
   [opts]
   (let [connection-data {:host (:host opts) :port (:port opts) :dn (:dn opts) :password (:password opts)}]
-    (loop [available-options list-query-cli-args]
-      (when (not= (count available-options) 0)
-        (if ((first available-options) opts)
-          (let [query-result (query-server connection-data gluequeries/glue-object-list-query (first available-options))]
+    ;handle list queries
+    (doall
+      (map
+        #(if (% opts)
+          (let [query-result (query-server connection-data gluequeries/glue-object-list-query %)]
             (println)
-            (println ((first available-options) query-descriptions))
-            (print-list
+            (println (% query-descriptions))
+            (print-double-column-list
               (if (= (:take-top opts) "0")
                 query-result
                 (take (Integer. (re-find #"\d+" (:take-top opts))) query-result)))
             (println)))
-        (recur (rest available-options))))
+        list-query-cli-args))
+    ;handle id query
     (if (:get-by-id opts)
-      (let [query-result (apply query-server (cons connection-data (cons gluequeries/glue-object-query (cons (:get-by-id opts) (split-attributes (:attributes opts))))))]
+      (let
+        [query-result
+         (apply query-server (cons connection-data (cons gluequeries/glue-object-query (cons (:get-by-id opts) (map #(keyword %) (split-attributes (:attributes opts)))))))]
         (println)
         (println (str "Selected attributes for objest with ID: " (:get-by-id opts)))
-        (if (= query-result nil)
+        (if (nil? query-result)
           (println "Database doesn't contain the given ID.")
-          (print-list (map #(str (name %) ": " (% query-result)) (keys query-result))))
+          (print-double-column-list query-result))
+        (println)))
+    ;handle foreign key queries
+    (if (:find-by-foreign-key opts)
+      (let [query-result (apply query-server (cons connection-data (cons gluequeries/glue-object-elements (cons (:find-by-foreign-key opts) (split-attributes (:specific-type opts))))))]
+        (println)
+        (println (str "Objects related to object with ID: " (:get-by-id opts)))
+        (if (nil? query-result)
+          (println "Database doesn't contain the given ID or no items are related to this object.")
+          (print-single-column-list query-result))
         (println)))
   ))
 
@@ -138,33 +151,34 @@
   "
   The application's main function
   "
-  ;
-  ; TODO: Add new args for objects and IDs
-  ;
   [& args]
   (let [[opts args banner]
-        (cli args
-          ["-h" "--help" "Show help" :flag true :default false]
-          ["-s" "--host" "GLUE LDAP host" :default "127.0.0.1"]
-          ["-p" "--port" "GLUE LDAP port" :default 389]
-          ["-d" "--dn" "Authentication user DN" :default "cn=admin,Mds-Vo-name=local,o=grid"] ; Wydaje mi się, że na czas samej pracy nad projektem, możęmy ustawić tutaj default, Michał
-          ["-w" "--password" "Authentication password"]
-          ["-ls" "--list-sites" "List GLUE Sites" :flag true :default false]
-          ["-lv" "--list-services" "List GLUE Services" :flag true :default false]
-          ["-lc" "--list-clusters" "List GLUE Clusters" :flag true :default false]
-          ["-lsc" "--list-subclusters" "List GLUE Subclusters" :flag true :default false]
-          ["-lce" "--list-ce" "List GLUE Computing Elements" :flag true :default false]
-          ["-lsf" "--list-software" "List GLUE Software" :flag true :default false]
-          ["-lse" "--list-se" "List GLUE Storage Elements" :flag true :default false]
-          ["-lsa" "--list-sa" "List GLUE Storage Areas" :flag true :default false]
-          ["-lvo" "--list-vo" "List GLUE Virtual Organizations" :flag true :default false]
-          ["-t" "--take-top" "Show only selected number of list query results (0 - take all)" :default "0"]
-          ["-g" "--get-by-id" "Get item by id"]
-          ["-a" "--attributes" "Add atributes to be listetd while querying by ID (i. e. \"GlueSiteEmailContact\")" :default ""]
-          )]
+    (cli args
+      ["-h" "--help" "Show help" :flag true :default false]
+      ["-s" "--host" "GLUE LDAP host" :default "127.0.0.1"]
+      ["-p" "--port" "GLUE LDAP port" :default 389]
+      ["-d" "--dn" "Authentication user DN" :default "cn=admin,Mds-Vo-name=local,o=grid"] ; Wydaje mi się, że na czas samej pracy nad projektem, możęmy ustawić tutaj default, Michał
+      ["-w" "--password" "Authentication password"]
+      ;list queries
+      ["-ls" "--list-sites" "List GLUE Sites" :flag true :default false]
+      ["-lv" "--list-services" "List GLUE Services" :flag true :default false]
+      ["-lc" "--list-clusters" "List GLUE Clusters" :flag true :default false]
+      ["-lsc" "--list-subclusters" "List GLUE Subclusters" :flag true :default false]
+      ["-lce" "--list-ce" "List GLUE Computing Elements" :flag true :default false]
+      ["-lsf" "--list-software" "List GLUE Software" :flag true :default false]
+      ["-lse" "--list-se" "List GLUE Storage Elements" :flag true :default false]
+      ["-lsa" "--list-sa" "List GLUE Storage Areas" :flag true :default false]
+      ["-lvo" "--list-vo" "List GLUE Virtual Organizations" :flag true :default false]
+      ;additional attribute
+      ["-t" "--take-top" "Show only selected number of list query results (0 - take all)" :default "0"]
+      ;id query
+      ["-g" "--get-by-id" "Get item by id"]
+      ["-a" "--attributes" "Specify atributes to be listetd while querying by ID (i. e. \"GlueSiteEmailContact\")" :default ""]
+      ;foreign key query
+      ["-f" "--find-by-foreign-key" "Find elements related to diven dn by foreign key"]
+      ["-c" "--specific-type" "Limits results of related object query to specific type" :default ""]
+      )]
 
-    ;(println "Ustawione argumenty uruchomienia:")
-    ;(println opts)
     ;;
     ;; Display help if -h was passed in the argument list, then exit
     ;;
--- src/gluebrowser/gluequeries.clj
+++ src/gluebrowser/gluequeries.clj
@@ -16,18 +16,15 @@
 ; for retrieving list of GLUE objects by specific type and
 ; limited to certain attributes
 ;
-(def glue-list-queries
-  {
-    :list-sites '("GlueSite" :GlueSiteUniqueID) ;{:filter "(objectClass=GlueSite)" :attributes [:GlueSiteUniqueID]}
-    :list-clusters '("GlueClusters" :GlueClusterUniqueID)
-    :list-subclusters '("GlueSubclusters" :GlueSubclusterUniqueID)
-    :list-ce '("GlueCE" :GlueCEUniqueID)
-    :list-software '("GlueSoftware" :GlueSoftware);wrong
-    :list-se '("GlueSE" :GlueSEUniqueID)
-    :list-sa '("GlueSA" :GlueSAUniqueID);wrong
-    :list-vo '("GlueVO" :GlueVOViewLocalID);??
-    :list-services '("GlueServices" :GlueServicesUniqueID)
-    })
+(def glue-list-queries {  :list-sites         '("GlueSite"        :GlueSiteUniqueID)
+                          :list-clusters      '("GlueCluster"     :GlueClusterUniqueID)
+                          :list-subclusters   '("GlueSubcluster"  :GlueSubClusterUniqueID)
+                          :list-ce            '("GlueCE"          :GlueCEUniqueID)
+                          :list-software      '("GlueSoftware"    :GlueSoftware) ;wrong TODO: find correct names
+                          :list-se            '("GlueSE"          :GlueSEUniqueID)
+                          :list-sa            '("GlueSA"          :GlueSALocalID)
+                          :list-vo            '("GlueVOInfo"      :GlueVOInfoLocalID)
+                          :list-services      '("GlueService"     :GlueServiceUniqueID)})
 
 
 (defn glue-object-list-query
@@ -43,31 +40,8 @@
         ldap-attribute (second (query glue-list-queries))]
     (info "Executing list query: " ldap-query " for attribute " ldap-attribute)
     (let [result (ldap/search ldap-server ldap-root-dn {:filter ldap-query :attributes [ldap-attribute]})]
-      (map ldap-attribute result))))
-
-;(defn get-object-by-id
-;  "
-;  Returns pure LDAP oobject by it's ID
-;  "
-;  [ldap-server id]
-;  (first (filter (complement nil?) (map
-;    #(try (ldap/get ldap-server (str (name (second (% glue-list-queries))) "=" id)) (catch Exception e nil))
-;    (keys glue-list-queries)))))
-;
-;(defn glue-object-query
-;  "
-;  Returns a GLUE object by ID with optionally limited set of attributes. If attrs is nil
-;  then it returns a map with all object's attributes returned by LDAP query.
-;  "
-;  [ldap-server id & attrs]
-;  (do
-;    (info (str "Executing object ID query for id: " id (if (not= (count attrs) 0) (str " with attributes: " (join " " attrs)) "") "."))
-;    (if-let [query-result (get-object-by-id ldap-server id)]
-;      (if (= (count attrs) 0)
-;        query-result
-;        (apply merge (map #(hash-map %1 (%1 query-result)) attrs)))
-;      (apply merge (map #(hash-map %1 nil) attrs)))
-;    ))
+      (map #(list (ldap-attribute %) (:dn %)) result))))
+;{:filter "(objectClass=GlueSite)" :attributes [:GlueSiteUniqueID]}
 
 (defn glue-object-query
   "
@@ -76,25 +50,16 @@
   "
   [ldap-server id & attrs]
   (do
-    (info (str "Executing object ID query for id: " id (if (not= (count attrs) 0) (str " with attributes: " (join " " attrs)) "") "."))
-    (try (if (= (count attrs) 0) (ldap/get ldap-server id) (ldap/get ldap-server id attrs)) (catch Exception e nil))))
+    (info (str "Executing object ID query for id: " id (if (not= (count attrs) 0) (str " with attributes: " (join ", " attrs)) "") "."))
+    (let
+      [query-result
+      (try
+        (if (zero? (count attrs))
+          (ldap/get ldap-server id)
+          (ldap/get ldap-server id attrs))
+        (catch Exception e {}))]
+      (map #(list (name %) (% query-result)) (keys query-result)))))
 
-;(defn glue-object-query
-;  "
-;  Returns a GLUE object by ID with optionally limited set of attributes. If attrs is nil
-;  then it returns a map with all object's attributes returned by LDAP query.
-;  "
-;  [ldap-server id & attrs]
-;  (do
-;  (info (str "Executing object ID query for id: " id (if (not= (count attrs) 0) (" with attributes: " (join " " attrs)) "") "."))
-;  (loop [unprocessed-basic-queries (keys glue-basic-queries)]
-;    (if (not= (count unprocessed-basic-queries) 0)
-;      (if-let
-;        [ldap-result
-;         (try (ldap/get ldap-server (str (name (second ((first unprocessed-basic-queries) glue-basic-queries))) "=" id)) (catch Exception e ))]
-;        ldap-result
-;        (recur (rest unprocessed-basic-queries)))
-;      {}))))
 
 (defn glue-object-elements
   "
@@ -106,6 +71,21 @@
   - GlueForeignKey: GlueSiteUniqueID=ARNES
 
   "
-  [ldap-server id type]
-  TODO)
-
+  [ldap-server id & types]
+  (do
+    (info (str "Executing foreign key query for id: " id (if (not= (count types) 0) (str " limited to type(s): " (join ", " types)) "") "."))
+    (let
+      [query-result
+      (ldap/search
+        ldap-server
+        ldap-root-dn
+        {:filter
+         (if (zero? (count types))
+           (str "(GlueForeignKey=" id ")")
+           (if (= 1 (count types))
+             (str "(&(objectClass=" (first types) ")(GlueForeignKey=" id "))")
+             (str "(&(|(objectClass=" (join ")(objectClass=" types) "))(GlueForeignKey=" id "))")))
+         :attributes
+         [:dn]})]
+      (map #(:dn %) query-result)
+      )))
